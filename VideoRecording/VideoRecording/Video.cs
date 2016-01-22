d8e978a9472ebe315ea937237040d6dd5343baf8
sandsmi <sandra.smietana@onet.eu>
2016-01-22 12:27:10 +0100

    Created the project, added functionality for saving files

---
+++ VideoRecording/VideoRecording/Video.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoRecording
+{
+    class Video
+    {
+        public string FileUrl { get; set; }
+        public string FileLocation { get; set; }
+        public bool IsCompressed { get; set; }
+    }
+}
---
+++ VideoRecording/VideoRecording/VideoViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows.Input;
+
+namespace VideoRecording
+{
+    class VideoViewModel : INotifyPropertyChanged
+    {
+        private readonly Video _video;
+        public VideoViewModel()
+        {
+            _video = new Video();
+        }
+        public string FileUrl
+        {
+            get { return _video.FileUrl; }
+            set
+            {
+                _video.FileUrl = value;
+                OnPropertyChanged("FileUrl");
+            }
+        }
+        public string FileLocation
+        {
+            get { return _video.FileLocation; }
+            set
+            {
+                _video.FileLocation = value;
+                OnPropertyChanged("FileLocation");
+            }
+        }
+        public bool IsCompressed
+        {
+            get { return _video.IsCompressed; }
+            set
+            {
+                _video.IsCompressed = value;
+                OnPropertyChanged("IsCompressed");
+            }
+        }
+
+        public ICommand DownloadFile { get { return new RelayCommand(DownloadFileExecute);  } }
+        public void DownloadFileExecute()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(FileUrl, FileLocation);
+            }
+        }
+
+        public ICommand Browse { get { return new RelayCommand(BrowseExecute);} }
+        public void BrowseExecute()
+        {
+            
+        }
+
+        private void Compression()
+        {
+            if (IsCompressed)
+                Process.Start("cmd", "ffmpeg -i 'rtsp://root:pass@172.16.1.200/profile2'  -vcodec copy -an test.mp4");
+            else
+                Process.Start("cmd", "ffmpeg -i 'rtsp://root:pass@172.16.1.200/profile2' -t 00:00:30 -c:v libx264 test.mp4");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
